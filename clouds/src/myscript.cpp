7206c209d7361ad3a595654e42763b11c6c4a765
Rajinish Bhatia <rajinishaneel@gmail.com>
2025-07-01 16:18:01 +0200

    rm readme

---
+++ clouds/src/myscript.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+class WorleyNoiseGenerator{
+private:
+    unsigned int SIZE;
+
+public:
+    WorleyNoiseGenerator(unsigned int SIZE)
+        :SIZE(SIZE){}
+
+    void generate(){
+        
+    }
+
+
+};
--- clouds2/README.md
+++
@@ -1,3 +0,0 @@
-![raym1](https://github.com/user-attachments/assets/60851350-96d7-4322-8dda-fe2cb9e79275)
-![raym2](https://github.com/user-attachments/assets/ba7862e2-bb53-494b-9085-e07406c7828d)
-
