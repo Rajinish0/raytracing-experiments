32cca596ffcc18172123053a8e7b104f7d8720a1
Rajinish Bhatia <rajinishaneel@gmail.com>
2025-07-01 23:31:31 +0200

    overkill

--- fromscratch/mat3.h
+++ fromscratch/mat3.h
@@ -3,14 +3,26 @@
 
 #include <istream>
 #include <ostream>
+#include <vec3.h>
 
 class mat3 {
 private:
     double m[9];
 
 public:
+    using coord = std::tuple<int, int>;
     mat3(){
         fill(0.);
+    }
+
+    mat3(double v){
+        fill(v);
+    }
+
+    mat3(vec3 v1, vec3 v2, vec3 v3){
+        for (int i =0; i < 3; ++i){
+            (*this) << v1[i] << v2[i] << v3[i];
+        }
     }
 
     inline size_t idx(size_t i, size_t j) const
@@ -54,6 +66,106 @@ public:
               +at(0, 2)*(at(1, 0) * at(2, 1) - at(2, 0) * at(1, 1) );
     }
 
+    double det_two_by_two(coord c1, coord c2, coord c3, coord c4)
+    {
+        auto &[i0, j0] = c1;
+        auto &[i1, j1] = c2;
+        auto &[i2, j2] = c3;
+        auto &[i3, j3] = c4;
+
+        return at(i0, j0)*at(i2, j2) - at(i1, j1)*at(i3, j3);
+    }
+
+    double determinant_at(size_t i, size_t j)
+    {
+        bool inc_j = (j == 1);
+        bool inc_i = (i == 1);
+
+        size_t si = (i > 0) ? 0 : i+1;
+        size_t sj = (j > 0) ? 0 : j+1;
+
+        double determ = at(si, sj) * at(si + inc_i + 1, sj + inc_j + 1) -
+                        at(si + 1 + inc_i, sj) * at(si, sj + 1 + inc_j);
+        return determ;
+    }
+
+    mat3 inv(){
+        mat3 adjoint;
+        for (int i =0; i < 3; ++i){
+            for (int j =0; j < 3; ++j){
+                //transpose
+                adjoint.at(j, i) = determinant_at(i, j) * ((i + j)%2 == 0 ? 1 : -1);
+            }
+        }
+
+        return adjoint;
+    }
+
+    mat3 inverse(){
+        //assumes non zero determinant!
+        #define _det det_two_by_two
+
+        mat3 out;
+        out << _det({1,1}, {1, 2}, {2, 1}, {2, 2}) 
+            << _det({0,2}, {0, 1}, {2, 2}, {2, 1})
+            << _det({0,1}, {0, 2}, {1, 1}, {1, 2})
+            << _det({1,2}, {1, 0}, {2, 2}, {2, 0})  
+            << _det({0,0}, {0, 2}, {2, 0}, {2, 2})
+            << _det({0,2}, {0, 0}, {1, 2}, {1, 0})  
+            << _det({1,0}, {1, 1}, {2, 0}, {2, 1}) 
+            << _det({0,1}, {0, 0}, {2, 1}, {2, 0}) 
+            << _det({0,0}, {0, 1}, {1, 0}, {1, 1})
+            ;
+
+        double determinent = this->det();
+        return out / determinent;
+    }
+
+    [[nodiscard]]
+    mat3 operator/(double v)
+    {
+        mat3 out;
+        for (int i =0; i < 9; ++i)
+            out << (m[i] / v);
+        return out;
+    }
+
+    mat3& operator/=(double v)
+    {
+        for (int i =0 ; i < 9; ++i)
+            m[i] /= v;
+
+        return *this;
+    }
+
+    [[nodiscard]]
+    mat3 operator*(double v)
+    {
+        mat3 out;
+        for (int i =0; i < 9; ++i)
+            out << m[i] * v;
+
+        return out;
+    }
+
+    mat3& operator*=(double v)
+    {
+        for (int i =0; i < 9; ++i)
+            m[i] *= v;
+        return *this;
+    }
+
+    [[nodiscard]]
+    vec3 operator*(vec3 vec)
+    {
+        vec3 out;
+        for (int i =0; i < 3; ++i)
+            for (int j =0; j < 3; ++j)
+                out[i] += at(i, j)*vec[j];
+
+        return out;
+    }
+
     double& operator[](size_t idx)
     {
         return m[idx];
@@ -74,10 +186,12 @@ public:
         return m[idx(i, j)];
     }
 
-    std::istream& operator>> (std::istream& is, double v)
+    mat3& operator<< (const double &v)
     {
         m[_cur_ind] = v;
         _cur_ind = (++_cur_ind) % 9;
+
+        return (*this);
     }
 
 private:
